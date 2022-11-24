60bf4980738dcf6c6de240fd1edd0dc444cde022
Julian <julicogui@gmail.com>
2022-11-24 19:44:32 -0300

    project setup

---
+++ src/cars/cars.service.ts
@@ -0,0 +1,38 @@
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { v4 as uuid } from 'uuid';
+import { Car } from './interfaces/car.interface';
+
+@Injectable()
+export class CarsService {
+  private cars: Car[] = [
+    {
+      id: uuid(),
+      brand: 'Toyota',
+      model: 'Corolla',
+    },
+    {
+      id: uuid(),
+      brand: 'Honda',
+      model: 'Civic',
+    },
+    {
+      id: uuid(),
+      brand: 'Audi',
+      model: 'TT',
+    },
+  ];
+
+  findAll() {
+    return this.cars;
+  }
+
+  findById(id: string) {
+    const car = this.cars.find((car) => car.id === id);
+
+    if (!car) {
+      throw new NotFoundException(`Car with id ${id} not found`);
+    }
+
+    return car;
+  }
+}
